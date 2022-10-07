42fb4b2be1f4695bb415889466f3a8c0929290d8
Aaron McAdam <aaron.mcadam@ticketmaster.com>
2022-10-07 22:56:22 +0100

    add navic back to lualine

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -3,7 +3,18 @@ if not status_ok then
   return
 end
 
+local status_navic_ok, navic = pcall(require, "nvim-navic")
+if not status_navic_ok then
+  return
+end
+
 lualine.setup({
+  sections = {
+    lualine_c = {
+      { 'filename' },
+      { navic.get_location, cond = navic.is_available },
+    }
+  },
   options = {
 		theme = "catppuccin"
   }
