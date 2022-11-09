f781519d9806a84aed66bedd214bf9608163320b
Aaron McAdam <aaron.mcadam@ticketmaster.com>
2022-11-09 15:02:14 +0000

    fix terminal keymaps

--- nvim/.config/nvim/lua/user/core/autocommands.lua
+++ nvim/.config/nvim/lua/user/core/autocommands.lua
@@ -1,6 +1,13 @@
 -- Fixes Autocomment
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  callback = function()
-    vim.cmd "set formatoptions-=cro"
-  end,
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+	end,
+})
+
+-- Highlight yanks
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
 })
--- nvim/.config/nvim/lua/user/plugins/configs/toggleterm.lua
+++ nvim/.config/nvim/lua/user/plugins/configs/toggleterm.lua
@@ -1,6 +1,6 @@
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
-  return
+	return
 end
 
 toggleterm.setup({
@@ -18,9 +18,19 @@ toggleterm.setup({
 	},
 })
 
+function _G.set_terminal_keymaps()
+	local opts = { noremap = true }
+
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-[>", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-u>", [[<C-\><C-n><C-u>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-d>", [[<C-\><C-n><C-d>]], opts)
+end
+
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
 function _LAZYGIT_TOGGLE()
-  lazygit:toggle()
+	lazygit:toggle()
 end
