773d29e92bc689c2cff228c7c318a7b4a094343f
Aaron McAdam <aaron.mcadam@ticketmaster.com>
2023-01-20 18:27:11 +0000

    tidy up mappings

--- nvim/.config/nvim/lua/user/core/keymaps.lua
+++ nvim/.config/nvim/lua/user/core/keymaps.lua
@@ -78,6 +78,7 @@ keymap("n", "<Leader>fw", require("telescope.builtin").grep_string, { desc = "[F
 keymap("n", "<Leader>fb", require("telescope.builtin").buffers, { desc = "[F]ind existing [B]uffers" })
 keymap("n", "<Leader>fd", require("telescope.builtin").diagnostics, { desc = "[F]ind [D]iagnostics" })
 keymap("n", "<Leader>fh", require("telescope.builtin").help_tags, { desc = "[F]ind [H]elp" })
+keymap("n", "<Leader>fk", require("telescope.builtin").keymaps, { desc = "[F]ind [K]eymapping" })
 keymap("n", "<Leader>fs", require("telescope.builtin").lsp_document_symbols, { desc = "[F]ind [S]ymbols" })
 keymap("n", "<Leader>fr", require("telescope.builtin").oldfiles, { desc = "[F]ind [R]ecently opened files" })
 -- Go to definition
