7a462ca5c10520ea1411ea1bef9ec81db802fe40
Aaron McAdam <aaron@aaronmcadam.com>
2023-06-23 19:23:25 +0100

    more features like surround

--- nvim/.config/nvim/lua/azvim/plugins/lsp.lua
+++ nvim/.config/nvim/lua/azvim/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
-	event = "BufReadPre",
+	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
--- nvim/.config/nvim/lua/azvim/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/azvim/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	event = "BufReadPost",
+	event = { "BufReadPost", "BufNewFile" },
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
