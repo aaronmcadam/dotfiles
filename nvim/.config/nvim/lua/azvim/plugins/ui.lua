e2944c31c1cc6fa8bc3ff804b57de27c0354f55c
Aaron McAdam <aaron@aaronmcadam.com>
2023-06-30 15:33:27 +0100

    oil

---
+++ nvim/.config/nvim/lua/azvim/plugins/ui.lua
@@ -0,0 +1,19 @@
+return {
+  -- better file browser
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    keys = {
+      {
+        "-",
+        "<cmd>lua require('oil').open()<CR>",
+        desc = "Open parent directory",
+      },
+    },
+  },
+}
