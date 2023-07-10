03fe8c8dbc50e08bafcb0588494244f5cff43de8
Aaron McAdam <aaron@aaronmcadam.com>
2023-07-10 12:34:39 +0100

    add fish aliases back in

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,13 @@
+fish_add_path "/opt/homebrew/bin"
+
+set -gx PATH "/opt/homebrew/bin:$PATH"
+set -gx VISUAL nvim
+set -gx EDITOR nvim
+set -gx GIT_EDITOR nvim
+set -gx AWS_SDK_LOAD_CONFIG 1
+# Configure lazy git configuration directory.
+set -gx CONFIG_DIR ~/.config/lazygit
+
 # https://github.com/asdf-vm/asdf
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
@@ -9,9 +19,53 @@ starship init fish | source
 
 set -gx SHELL "/opt/homebrew/bin/fish"
 
-# pnpm
-set -gx PNPM_HOME "/Users/Aaron.Mcadam/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
+# keybindings
+# General
+alias ... "cd ../.."
+alias ag "rg"
+alias c "clear"
+alias cat "bat"
+alias cd "z"
+alias change-bindings "v ~/.config/omf/key_bindings.fish"
+alias update-brewfile "brew bundle dump -f"
+alias co "code"
+alias fr "fish"
+alias l "exa -la"
+alias lg "lazygit"
+alias ls "exa -la"
+alias r "rm"
+alias rr "rm -rf"
+alias v "nvim"
+alias v. "nvim"
+alias update-vim-plugins "nvim --headless '+Lazy! sync' +qa"
+alias zz "z -"
+
+# JavaScript
+abbr -a n "npm install"
+abbr -a nt "npm test"
+abbr -a ntw "npm test -- --watch"
+abbr -a nx "pnpm nx"
+abbr -a y "yarn"
+abbr -a yd "yarn dev"
+abbr -a ys "yarn start"
+abbr -a ysb "yarn storybook"
+abbr -a yt "yarn test"
+abbr -a ytw "yarn test --watch"
+abbr -a p "pnpm"
+abbr -a pi "pnpm i"
+abbr -a pd "pnpm dev"
+abbr -a ps "pnpm start"
+abbr -a psb "pnpm storybook"
+abbr -a pt "pnpm test"
+abbr -a ptw "pnpm test --watch"
+
+# Git
+alias gdb "git branch | fzf --header 'Delete Branch' --reverse -m --pointer='' | xargs -n 1 git branch -D"
+alias gcr "git branch --sort=-committerdate | fzf --header 'Checkout Recent Branch' --preview 'git diff --color=always {1}' --reverse -m --pointer='' | xargs -n 1 git checkout"
+abbr -a grbi git rebase --interactive
+abbr -a grbim git rebase --interactive main
+abbr -a grbil git rebase --interactive HEAD~2
+abbr -a s "git status -s"
+abbr -a h "gh"
+abbr -a hb "gh browse"
+abbr -a hc "gh repo clone"
