76293e3c01f03371b320db05157a2223133ef76a
Aaron McAdam <aaron@aaronmcadam.com>
2024-08-15 13:25:39 +0100

    use wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,69 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+config.font_size = 18
+
+local regular_font = "Monaspace Neon"
+local italic_font = "Monaspace Radon"
+config.font = wezterm.font({ family = regular_font, weight = "Regular" })
+config.font_rules = {
+	-- For Bold-but-not-italic text, use this relatively bold font, and override
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font({
+			family = regular_font,
+			weight = "Bold",
+		}),
+	},
+
+	-- Bold-and-italic
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({
+			family = italic_font,
+			weight = "Bold",
+			italic = true,
+		}),
+	},
+
+	-- normal-intensity-and-italic
+	{
+		intensity = "Normal",
+		italic = true,
+		font = wezterm.font({
+			family = italic_font,
+			weight = "Regular",
+			italic = true,
+		}),
+	},
+
+	-- half-intensity-and-italic (half-bright or dim); use a lighter weight font
+	{
+		intensity = "Half",
+		italic = true,
+		font = wezterm.font({
+			family = italic_font,
+			weight = "Light",
+			italic = true,
+		}),
+	},
+
+	-- half-intensity-and-not-italic
+	{
+		intensity = "Half",
+		italic = false,
+		font = wezterm.font({
+			family = regular_font,
+			weight = "Light",
+		}),
+	},
+}
+
+config.enable_tab_bar = false
+config.window_background_opacity = 0.9
+
+return config
