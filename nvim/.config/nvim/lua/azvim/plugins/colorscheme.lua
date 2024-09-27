3ab441bc4298be1e1b7390e30cfa9f6b5120a952
Aaron McAdam <aaron@aaronmcadam.com>
2024-09-27 19:02:58 +0100

    colorscheme: remove settings that are already defaults

--- nvim/.config/nvim/lua/azvim/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/azvim/plugins/colorscheme.lua
@@ -6,28 +6,21 @@ return {
     opts = {
       transparent_background = true,
       integrations = {
-        alpha = true,
         fidget = true,
-        flash = true,
-        gitsigns = true,
         harpoon = true,
         lsp_trouble = true,
-        markdown = true,
         mason = true,
         navic = {
           enabled = true,
         },
         noice = true,
         neotest = true,
-        notify = true,
         octo = true,
         overseer = true,
-        render_markdown = true,
         telescope = {
           enabled = true,
           style = "nvchad",
         },
-        treesitter = true,
         which_key = true,
       },
       custom_highlights = function(colors)
