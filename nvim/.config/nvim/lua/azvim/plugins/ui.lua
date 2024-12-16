a14dc0b90264d5d5c32e86bcd7c27204a82f10db
Aaron McAdam <aaron@aaronmcadam.com>
2024-12-16 11:34:15 +0000

    try snacks.nvim

--- nvim/.config/nvim/lua/azvim/plugins/ui.lua
+++ nvim/.config/nvim/lua/azvim/plugins/ui.lua
@@ -285,18 +285,22 @@ return {
     },
   },
 
-  -- indent guides for Neovim
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = require("azvim.plugins.configs.indent-blankline").opts,
-  },
-
   -- Nicer UI
   {
-    "stevearc/dressing.nvim",
-    opts = {},
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- dashboard = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      -- words = { enabled = true },
+    },
   },
   {
     "folke/noice.nvim",
