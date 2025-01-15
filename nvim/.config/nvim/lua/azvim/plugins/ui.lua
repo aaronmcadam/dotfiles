106748b94ef2bdc8aaa3d987eced35c6463e5f79
Aaron McAdam <aaron@aaronmcadam.com>
2025-01-15 17:16:20 +0000

    remove comment

--- nvim/.config/nvim/lua/azvim/plugins/ui.lua
+++ nvim/.config/nvim/lua/azvim/plugins/ui.lua
@@ -298,7 +298,6 @@ return {
       input = { enabled = true },
       notifier = { enabled = true },
       quickfile = { enabled = true },
-      -- scroll = { enabled = true },
       statuscolumn = { enabled = true },
       -- words = { enabled = true },
     },
