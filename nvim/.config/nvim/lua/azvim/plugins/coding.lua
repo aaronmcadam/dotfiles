ce7d3cfaf23450873d5481832e9f2426cb6bdf94
Aaron McAdam <aaron@aaronmcadam.com>
2025-01-29 21:32:25 +0000

    blink.cmp: avante commands and snippets

--- nvim/.config/nvim/lua/azvim/plugins/coding.lua
+++ nvim/.config/nvim/lua/azvim/plugins/coding.lua
@@ -147,6 +147,9 @@ return {
           "lsp",
           "buffer",
           "path",
+          "avante_commands",
+          "avante_mentions",
+          "avante_files",
         },
         -- Disable cmdline completions
         cmdline = {},
@@ -162,6 +165,24 @@ return {
           obsidian_tags = {
             name = "obsidian_tags",
             module = "blink.compat.source",
+          },
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_files",
+            module = "blink.compat.source",
+            score_offset = 100, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000, -- show at a higher priority than lsp
+            opts = {},
           },
           lsp = {
             enabled = function()
