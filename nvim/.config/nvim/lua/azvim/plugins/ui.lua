5c19a2ee1eb4f9d7949f0ce211895f81ec4b0bd5
Aaron McAdam <aaron@aaronmcadam.com>
2025-02-03 20:38:40 +0000

    latest neovim

--- nvim/.config/nvim/lua/azvim/plugins/ui.lua
+++ nvim/.config/nvim/lua/azvim/plugins/ui.lua
@@ -536,15 +536,21 @@ return {
     "preservim/vim-pencil",
   },
 
-  -- Build better Neovim habits
-  -- lazy.nvim
-  -- {
-  --   "m4xshen/hardtime.nvim",
-  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  --   opts = {},
-  -- },
-  -- {
-  --   "tris203/precognition.nvim",
-  --   opts = {},
-  -- },
+  -- LSP outline sidebar
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>mo", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      symbol_folding = {
+        -- Unfold entire symbol tree by default with false, otherwise enter a
+        -- number starting from 1
+        autofold_depth = false,
+        -- autofold_depth = 1,
+      },
+    },
+  },
 }
