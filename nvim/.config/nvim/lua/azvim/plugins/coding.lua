ffbb9a0ecb9d6ee3742f1ef7cfb61b81fa1bb2f9
Aaron McAdam <aaron@aaronmcadam.com>
2025-02-11 10:50:57 +0000

    latest

--- nvim/.config/nvim/lua/azvim/plugins/coding.lua
+++ nvim/.config/nvim/lua/azvim/plugins/coding.lua
@@ -435,6 +435,7 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+    lazy = false,
     config = function()
       require("refactoring").setup()
     end,
