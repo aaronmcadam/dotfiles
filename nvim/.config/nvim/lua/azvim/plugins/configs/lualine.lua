58bfc827eda177cc35eb5067e6ed7f3ae88bbadc
Aaron McAdam <aaron@aaronmcadam.com>
2025-02-26 14:43:53 +0000

    add neotest status to status line

--- nvim/.config/nvim/lua/azvim/plugins/configs/lualine.lua
+++ nvim/.config/nvim/lua/azvim/plugins/configs/lualine.lua
@@ -1,5 +1,49 @@
 local M = {}
 
+-- inspired by https://github.com/nvim-neotest/neotest/pull/217#issuecomment-1454851348
+local neotest_status = function()
+  local status_ok, neotest = pcall(require, "neotest")
+  if not status_ok then
+    return ""
+  end
+  local adapters = neotest.state.adapter_ids()
+  if #adapters > 0 then
+    local status = neotest.state.status_counts(adapters[1], {
+      buffer = vim.api.nvim_buf_get_name(0),
+    })
+    local sections = {
+      {
+        sign = "",
+        count = status.failed,
+        base = "NeotestFailed",
+        tag = "test_fail",
+      },
+      {
+        sign = "",
+        count = status.running,
+        base = "NeotestRunning",
+        tag = "test_running",
+      },
+      {
+        sign = "",
+        count = status.passed,
+        base = "NeotestPassed",
+        tag = "test_pass",
+      },
+    }
+
+    local result = {}
+    for _, section in ipairs(sections) do
+      if section.count > 0 then
+        table.insert(result, "%#" .. section.base .. "#" .. section.sign .. " " .. section.count)
+      end
+    end
+
+    return table.concat(result, " ")
+  end
+  return ""
+end
+
 function M.opts()
   local helpers = require("azvim.core.helpers")
 
@@ -44,6 +88,7 @@ function M.opts()
         { "fancy_macro" },
         { "fancy_diagnostics" },
         { "fancy_searchcount" },
+        { neotest_status },
         {
           require("package-info").get_status,
           color = helpers.fg("Statement"),
